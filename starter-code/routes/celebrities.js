640e9ab358a0de880478096d30a8f153ab751562
Laia Lloret <laialloret95@gmail.com>
2021-04-26 21:41:48 +0200

    delete and half update celebrities

--- starter-code/routes/celebrities.js
+++ starter-code/routes/celebrities.js
@@ -9,11 +9,46 @@ router.get("/celebrities", (req,res) => {
   .catch(err => console.log('error', err))
 })
 
+router.get("/celebrities/new", (req,res) => {
+  res.render('celebrities/new');
+})
+
 router.get("/celebrities/:id", (req, res) => {
   const {id} = req.params
   Celebrity.findById(id)
     .then(celebrity => {
       res.render('celebrities/show', celebrity)
+    })
+    .catch(err => { console.log('error', err) })
+});
+
+router.post("/celebrities", (req,res) => {
+  const { name, occupation, catchPhrase } = req.body;
+  Celebrity.create({name, occupation, catchPrhase})
+    .then(celebrityDB => {
+      res.redirect('/celebrities')
+    })
+    .catch(err => {
+      res.redirect("/celebrities/new");
+      console.log('error', err)})
+})
+
+router.post("/celebrities/:id/delete", (req,res) => {
+  const { id } = req.params
+  Celebrity.findByIdAndDelete(id)
+    .then(celebrity => {
+      console.log(`We just killed ${celebrity}`)
+      res.redirect('/celebrities')
+    })
+    .catch(err => { console.log(err) })
+})
+
+router.get("/celebrities/:id/edit", (req, res) => {
+  const {id} = req.params
+  Celebrity.findById(id)
+    .then(celebrity => {
+      console.log(celebrity)
+      res.render('celebrities/edit', celebrity)
     })
     .catch(err => { console.log('error', err) })
 })
